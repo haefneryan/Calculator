f8d3e3601b0f3ff4fa2d394d9005ce0d6919190e
Ryan Haefner <haefner.ryan@gmail.com>
2021-11-11 11:02:31 -0500

    _

--- Calculator2.js
+++ Calculator2.js
@@ -30,6 +30,12 @@ function clr(){
 
 // Function stores theme used in last session and applies styling
 function storemode(){
+<<<<<<< HEAD
+=======
+	if (localStorage.getItem('saved_theme') == null) {
+		localStorage.setItem('saved_theme', document.getElementById('body').className);
+	}
+>>>>>>> parent of c8f28f6 (initial commit)
 	document.getElementById('body').className = localStorage.getItem('saved_theme');
 	console.log(localStorage.getItem('saved_theme'));
 }
